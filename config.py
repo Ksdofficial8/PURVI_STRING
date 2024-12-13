a1db3d804c559d42f504014fe100e33062048b1c
Ksdofficial8 <ksdofficial84@gmail.com>
2024-12-13 14:58:30 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ load_dotenv()
 API_ID = int(getenv("API_ID","13404637"))
 API_HASH = getenv("API_HASH","a069bf02806468fe18427ab6b9a3bb6c")
 
-BOT_TOKEN = getenv("BOT_TOKEN","7158045860:AAFK9c1BRAQd2fM2Zyctbiz4y-iDFMVe8Rc")
+BOT_TOKEN = getenv("BOT_TOKEN","7158045860:AAFo_KzbgrSbhQP9pCsw4gHBSB21N7AMhl8")
 OWNER_ID = int(getenv("OWNER_ID","6283192619"))
 
 MONGO_DB_URI = getenv("MONGO_DB_URI","mongodb+srv://DxLEGEND143:DxLEGEND143@dxlegend.oztipqk.mongodb.net/?retryWrites=true&w=majority&appName=DxLEGEND")
