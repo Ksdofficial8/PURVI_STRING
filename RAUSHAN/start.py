65f0b39793dc45a9b6e57d0a2a49504d1470c423
Ksdofficial8 <ksdofficial84@gmail.com>
2024-12-13 15:06:18 +0530

    Update start.py

--- RAUSHAN/start.py
+++ RAUSHAN/start.py
@@ -24,7 +24,7 @@ async def start(bot: Client, msg: Message):
         reply_markup=InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton(text="˹ ɢᴇɴᴇʀᴀᴛᴇ sᴛʀɪɴɢ ˼", callback_data="generate")
+                    InlineKeyboardButton(text="˹ ɢᴇɴᴇʀᴀᴛᴇ sᴛʀɪɴɢ ˼", url="https://telegram.tools/session-string-generator"),
                 ],
                 [
                     InlineKeyboardButton("˹ sᴜᴘᴘᴏʀᴛ ˼", url="https://t.me/TEAM_RIYA_SUPPORT"),
